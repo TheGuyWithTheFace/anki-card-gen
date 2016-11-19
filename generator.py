720a458bb5fd3e4765f1b4eb9836c0415514456e
Perry Cate <NPCate01@gmail.com>
2016-11-18 22:55:36 -0500

    Add semi-accurate tone counting
    Does not pick up on neutral tones, will print an error message in this
    case.

--- generator.py
+++ generator.py
@@ -15,8 +15,6 @@ def main():
     f = open("input.csv", newline='')
     parse_input(f)
 
-    print(_mappings)
-
 
 def parse_input(input_file):
     """Reads input "csv" (not a real csv), returns list of dictionaries."""
@@ -24,27 +22,24 @@ def parse_input(input_file):
     vocab = []
 
     for line in list(input_file):
-        items = line.split(',')
+        items = [x.strip() for x in line.split(',')]
         # Problem: items may have multiple definitions separated by commas
         # Solution: Look for (hopefully unique) Chinese character + pinyin combination
         #    in items. pinyin is always the next item after the character, and should
         #    always have a tone_count equal to the number of characters in chinese
 
 
-        character_index = 1 # delete me
-        """
         character_index = -1
-        for i in range(items-1):
-            first = items[i]
-            second = items[i+1]
+        for i in range(len(items)-1):
+            first = items[i].strip()
+            second = items[i+1].strip()
             if(len(first) == tone_count(second)):
                 character_index = i
                 break
         
-        if(character_index = -1):
-            print(line)
-            break
-        """
+        if(character_index == -1):
+            print("Error parsing line: " + line)
+            continue
 
         term = {}
         # meaning must be every value before the character 
@@ -71,14 +66,24 @@ def format_pinyin(word):
     for c in word:
         if(c in _mappings):
             word = word.replace(c, _mappings[c])
-            print("replace")
 
     return word 
 
 # We will use this to determine where everything else is in each line of input
 def tone_count(word):
     """Returns the number of characters in word that are used for pinyin tone encoding"""
-    pass  #TODO
+    count = 0
+    global _mappings
+
+    # words describing lesson count and page # give incorrect input since they have numbers
+    if "L. " in word or "p. " in word:
+        return 0
+
+    for c in word:
+        if c in _mappings and "L" not in word:
+            count += 1
+
+    return count
 
 def load_mappings(filename):
     mapfile = open(filename, "r")
